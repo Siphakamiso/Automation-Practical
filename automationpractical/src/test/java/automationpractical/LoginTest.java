f8b91a9dc7ce5588082c4c6da38f95f3c5b769d2
Siphakamiso <Sphakamisok@yahoo.com>
2020-12-15 21:12:09 +0200

    Adding final changes

--- automationpractical/src/test/java/automationpractical/LoginTest.java
+++ automationpractical/src/test/java/automationpractical/LoginTest.java
@@ -4,19 +4,22 @@ import org.junit.Test;
 
 import automationpractical.Pages.HomePage;
 import automationpractical.Pages.LoginPage;
+import automationpractical.Utilities.GlobalVariables;
 
 public class LoginTest extends BaseTest {
     HomePage homePage;
     LoginPage loginPage;
+    GlobalVariables globalvariables;
 
     @Test
     public void logInTest(){
         homePage = new HomePage(driver);
         loginPage = new LoginPage(driver);
+        globalvariables = new GlobalVariables(driver);
         homePage.goToLoginPage();
 
-        loginPage.setEmailText("emailText");
-        loginPage.setPasswrdText("PasswrdText");
+        loginPage.setEmailText(globalvariables.username);
+        loginPage.setPasswrdText(globalvariables.password);
         loginPage.clickSigninBtn();
     }
 }
